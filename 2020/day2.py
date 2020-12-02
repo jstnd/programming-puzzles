beed4217abd552656b335b81e47bd5c30ce7e492
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-02 13:38:07 -0500

    Add Day 2 2020 Part 2

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,18 +1,43 @@
-def occurrences(string, char):
-    return len([i for i, letter in enumerate(string) if letter == char])
+import time
+
+
+def part1():
+    file = open("day2input.txt", "r")
+    valid = 0
+
+    for ln in file:
+        line = ln.replace(":", "").replace("-", " ").strip("\n").split(" ")
+        if int(line[1]) >= len([i for i, letter in enumerate(line[3]) if letter == line[2]]) >= int(line[0]):
+            valid += 1
+
+    return valid
+
+
+def part2():
+    file = open("day2input.txt", "r")
+    valid = 0
+
+    for ln in file:
+        temp = 0
+        line = ln.replace(":", "").replace("-", " ").strip("\n").split(" ")
+        if (line[3])[int(line[0]) - 1] == line[2]:
+            temp += 1
+
+        if (line[3])[int(line[1]) - 1] == line[2]:
+            temp += 1
+
+        if temp == 1:
+            valid += 1
+
+    return valid
 
 
 def main():
-    valid = 0
+    start = time.time()
+    print(f"Part 1: {part1()}; Time: {time.time() - start}")
 
-    f = open("day2input.txt", "r")
-
-    for ln in f:
-        line = ln.replace(":", "").replace("-", " ").strip("\n").split(" ")
-        if int(line[1]) >= occurrences(line[3], line[2]) >= int(line[0]):
-            valid += 1
-
-    print(valid)
+    start = time.time()
+    print(f"Part 2: {part2()}; Time: {time.time() - start}")
 
 
 if __name__ == "__main__":
