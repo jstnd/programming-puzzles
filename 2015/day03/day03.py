95fc604244a196efb98075081e1d02fe880e21f7
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-04 18:17:37 -0500

    Modify day 3 2015 solutions

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -1,53 +1,26 @@
+moves = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
+
+
 def part1(dirs):
-    current = [0, 0]
-    visited = {(current[0], current[1])}
+    current = (0, 0)
+    visited = {current}
 
     for d in dirs:
-        if d == "^":
-            current[1] += 1
-        elif d == "v":
-            current[1] -= 1
-        elif d == ">":
-            current[0] += 1
-        else:
-            current[0] -= 1
-
-        if (current[0], current[1]) not in visited:
-            visited.add((current[0], current[1]))
+        visited.add(current := tuple(map(sum, zip(current, moves[d]))))
 
     return len(visited)
 
 
 def part2(dirs):
-    santa = [0, 0]
-    robo = [0, 0]
-    visited = {(santa[0], santa[1])}
+    santa = (0, 0)
+    robo = (0, 0)
+    visited = {santa}
 
     for i, d in enumerate(dirs):
         if i % 2 == 0:
-            if d == "^":
-                santa[1] += 1
-            elif d == "v":
-                santa[1] -= 1
-            elif d == ">":
-                santa[0] += 1
-            else:
-                santa[0] -= 1
-
-            if (santa[0], santa[1]) not in visited:
-                visited.add((santa[0], santa[1]))
+            visited.add(santa := tuple(map(sum, zip(santa, moves[d]))))
         else:
-            if d == "^":
-                robo[1] += 1
-            elif d == "v":
-                robo[1] -= 1
-            elif d == ">":
-                robo[0] += 1
-            else:
-                robo[0] -= 1
-
-            if (robo[0], robo[1]) not in visited:
-                visited.add((robo[0], robo[1]))
+            visited.add(robo := tuple(map(sum, zip(robo, moves[d]))))
 
     return len(visited)
 
