d51129d1b2ebbef6e2b1232cd3310e9730f28c73
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-04 18:41:00 -0500

    Modify day 3 2015 solutions even more

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -2,27 +2,21 @@ moves = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
 
 
 def part1(dirs):
-    current = (0, 0)
-    visited = {current}
-
-    for d in dirs:
-        visited.add(current := tuple(map(sum, zip(current, moves[d]))))
-
-    return len(visited)
+    return len(deliver(dirs))
 
 
 def part2(dirs):
-    santa = (0, 0)
-    robo = (0, 0)
-    visited = {santa}
+    return len(deliver(dirs[::2]) | deliver(dirs[1::2]))
 
-    for i, d in enumerate(dirs):
-        if i % 2 == 0:
-            visited.add(santa := tuple(map(sum, zip(santa, moves[d]))))
-        else:
-            visited.add(robo := tuple(map(sum, zip(robo, moves[d]))))
 
-    return len(visited)
+def deliver(dirs):
+    pos = (0, 0)
+    visited = {pos}
+
+    for d in dirs:
+        visited.add(pos := tuple(map(sum, zip(pos, moves[d]))))
+
+    return visited
 
 
 chars = [ch for ch in open("input03.txt").read()]
