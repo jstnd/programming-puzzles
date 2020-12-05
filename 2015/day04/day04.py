dddaa32b7bf826bc11f92436256723817e67b5e7
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-04 23:57:32 -0500

    Modify day 4 2015 solution

--- 2015/day04/day04.py
+++ 2015/day04/day04.py
@@ -2,11 +2,9 @@ import hashlib
 
 
 def find(msg, start):
-    count = 0
-    while True:
+    count = 1
+    while hashlib.md5((msg + str(count)).encode()).hexdigest()[:len(start)] != start:
         count += 1
-        if hashlib.md5((msg + str(count)).encode()).hexdigest()[:len(start)] == start:
-            break
 
     return count
 
