b1a33bd00aa23da530ff6eae58ced6fdac311bc4
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-05 16:02:58 -0500

    Add regex versions of day 5 2015 solutions

--- 2015/day05/day05.py
+++ 2015/day05/day05.py
@@ -1,3 +1,5 @@
+import re
+
 
 def find_pair(string):
     for i in range(len(string) - 3):
@@ -19,5 +21,7 @@ def find_repeat(string):
 strings = [ln.strip() for ln in open("input05.txt").readlines()]
 
 print(len([s for s in strings if sum(s.count(c) for c in "aeiou") >= 3 and sum(s.count(c * 2) for c in "abcdefghijklmnopqrstuvwxyz") >= 1 and sum(s.count(c) for c in {"ab", "cd", "pq", "xy"}) == 0]))  # part 1 - 255
+print(len([s for s in strings if re.search(r"(.*[aeiou]){3}", s) and re.search(r"(.)\1", s) and not re.search(r"(ab|cd|pq|xy)", s)]))  # regex version
 
 print(len([s for s in strings if find_pair(s) and find_repeat(s)]))  # part 2 - 55
+print(len([s for s in strings if re.search(r"(..).*\1", s) and re.search(r"(.).\1", s)]))  # regex version
