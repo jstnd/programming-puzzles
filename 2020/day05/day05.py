d8136d4663cbbf98d8286b0059d6ec059a367365
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-05 14:30:14 -0500

    Update day 5 2020 solution

--- 2020/day05/day05.py
+++ 2020/day05/day05.py
@@ -1,25 +1,16 @@
-def binary(high, low, chars):
+def binary(low, high, chars):
     for c in chars:
-        if c == "F" or c == "L":
-            high -= ((high - low) + 1) // 2
+        if c in "FL":
+            high = (high + low) // 2
         else:
-            low += ((high - low) + 1) // 2
+            low = (high + low) // 2 + 1
 
     return high
 
 
 seats = [ln.strip() for ln in open("input05.txt").readlines()]
 
-ids = []
-highest = 0
-for p in seats:
-    ids.append(seat := binary(127, 0, p[:7]) * 8 + binary(7, 0, p[7:]))
-    if seat > highest:
-        highest = seat
+print(max(ids := set(binary(0, 127, p[:7]) * 8 + binary(0, 7, p[7:]) for p in seats)))  # part 1 - 951
+# print(max(ids := set(int("".join("0" if c in "FL" else "1" for c in s), 2) for s in seats)))  # alternate part 1
 
-print(highest)
-
-for i in range(ids[0], ids[-1]):
-    if i not in ids:
-        print(i)
-        break
+print(([i for i in range(min(ids), max(ids) + 1) if i not in ids])[0])  # part 2 - 653
