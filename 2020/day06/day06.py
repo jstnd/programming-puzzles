e1363925daec7bf160bc023f6c8d5531ed91f893
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-06 02:47:29 -0500

    Update day 6 2020

--- 2020/day06/day06.py
+++ 2020/day06/day06.py
@@ -1,3 +1,3 @@
-inp = [ln for ln in open("input06.txt").read().split("\n\n")]
-print(sum(len(set(g.replace("\n", ""))) for g in inp))  # part 1
-print(sum(len(set.intersection(*[set(p) for p in g.split("\n")])) for g in inp))  # part 2
+inp = open("input06.txt").read().split("\n\n")
+print(sum(len(set(g.replace("\n", ""))) for g in inp))  # part 1 - 6683
+print(sum(len(set.intersection(*map(set, g.split("\n")))) for g in inp))  # part 2 - 3122
