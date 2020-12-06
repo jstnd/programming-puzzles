fd962fbd75e7293ad79efc5e614c32a8da97d85c
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-06 02:56:31 -0500

    Update day 3 2015

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -6,7 +6,7 @@ def deliver(ds):
     return set((pos := tuple(map(sum, zip(pos, moves[d])))) for d in ds) | {(0, 0)}
 
 
-dirs = [ch for ch in open("input03.txt").read()]
+dirs = open("input03.txt").read()
 
 print(len(deliver(dirs)))  # part 1 - 2572
 print(len(deliver(dirs[::2]) | deliver(dirs[1::2])))  # part 2 - 2631
