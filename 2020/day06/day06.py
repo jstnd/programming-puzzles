467e913575e577b7fe55829d3483f3db1c35d73b
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-06 00:37:21 -0500

    Fix slight format issue

--- 2020/day06/day06.py
+++ 2020/day06/day06.py
@@ -1,4 +1,3 @@
-
 inp = [ln for ln in open("input06.txt").read().split("\n\n")]
 print(sum(len(set(g.replace("\n", ""))) for g in inp))  # part 1
 print(sum(len(set.intersection(*[set(p) for p in g.split("\n")])) for g in inp))  # part 2
