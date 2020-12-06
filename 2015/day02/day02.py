785cf3dffb0efcab1656d469f527d574a4e28813
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-06 02:58:51 -0500

    Update day 2 2015

--- 2015/day02/day02.py
+++ 2015/day02/day02.py
@@ -1,7 +1,7 @@
 import heapq
 import math
 
-boxes = [list(map(int, ln.split("x"))) for ln in open("input02.txt").read().splitlines()]
+boxes = [list(map(int, ln.split("x"))) for ln in open("input02.txt").readlines()]
 
 print(sum(2 * sum(a := [b[x] * b[y] for x, y in [(0, 1), (1, 2), (0, 2)]]) + min(a) for b in boxes))  # part 1 - 1586300
 print(sum(2 * sum(heapq.nsmallest(2, b)) + math.prod(b) for b in boxes))  # part 2 - 3737498
