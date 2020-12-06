9a355a2e3200de666c454972157da39208ce6767
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-06 03:40:09 -0500

    Update day 1 2015

--- 2015/day01/day01.py
+++ 2015/day01/day01.py
@@ -1,21 +1,6 @@
 import itertools
 
+chars = [1 if ch == "(" else -1 for ch in open("input01.txt").read()]
 
-def part1(chs):
-    return sum(1 if c == "(" else -1 for c in chs)
-
-
-def part2(chs):
-    return list(itertools.accumulate([1 if c == "(" else -1 for c in chs])).index(-1) + 1
-
-    # floor = 0
-    # for i, c in enumerate(chs):
-    #     floor += 1 if c == "(" else -1
-    #     if floor == -1:
-    #         return i + 1
-
-
-chars = [ch for ch in open("input01.txt").read()]
-
-print(part1(chars))  # 138
-print(part2(chars))  # 1771
+print(sum(chars))  # part 1 - 138
+print(list(itertools.accumulate(chars)).index(-1) + 1)  # part 2 - 1771
