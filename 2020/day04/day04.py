ba9c2ffcc4242c4a8cf24ac1798b6d91658100e3
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-08 15:54:15 -0500

    Simplify day 4 2020

--- 2020/day04/day04.py
+++ 2020/day04/day04.py
@@ -1,58 +1,16 @@
 import re
 
+REGEX = {
+    "byr": "^(19[2-9]\d|200[0-2])$",
+    "iyr": "^20(1\d|20)$",
+    "eyr": "^20(2\d|30)$",
+    "hgt": "^(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)$",
+    "hcl": "^#[\d|(a-f)]{6}$",
+    "ecl": "^(amb|blu|brn|gry|grn|hzl|oth)$",
+    "pid": "^\d{9}$"
+}
 
-def part1(ports):
-    valid = 0
+passports = [p.replace("\n", " ").split() for p in open("input04.txt").read().split("\n\n")]
 
-    for p in ports:
-        current = p.split()
-        valid += len(current) == 8 or (len(current) == 7 and not [s for s in current if s.startswith("cid")])
-
-    return valid
-
-
-def part2(ports):
-    valid = 0
-
-    for p in ports:
-        current = p.split()
-        if len(current) == 8 or (len(current) == 7 and not [s for s in current if s.startswith("cid")]):
-            valid += is_valid_passport(current)
-
-    return valid
-
-
-def is_valid_passport(port):
-    eye_colors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-
-    for e in port:
-        field = e.split(":")
-        if field[0] == "byr":
-            if not (len(field[1]) == 4 and 1920 <= int(field[1]) <= 2002):
-                return False
-        elif field[0] == "iyr":
-            if not (len(field[1]) == 4 and 2010 <= int(field[1]) <= 2020):
-                return False
-        elif field[0] == "eyr":
-            if not (len(field[1]) == 4 and 2020 <= int(field[1]) <= 2030):
-                return False
-        elif field[0] == "hgt":
-            if not (((field[1])[-2:] == "cm" and 150 <= int((field[1])[:-2]) <= 193)
-                    or ((field[1])[-2:] == "in" and 59 <= int((field[1])[:-2]) <= 76)):
-                return False
-        elif field[0] == "hcl":
-            if not re.match("^#[\d|(a-f)]{6}$", field[1]):
-                return False
-        elif field[0] == "ecl":
-            if not field[1] in eye_colors:
-                return False
-        elif field[0] == "pid":
-            if not re.match("^\d{9}$", field[1]):
-                return False
-
-    return True
-
-
-passports = [p.replace("\n", " ") for p in open("input04.txt").read().split("\n\n")]
-print(part1(passports))  # 256
-print(part2(passports))  # 198
+print(sum(sum(1 if f[:3] != "cid" else 0 for f in p) == 7 for p in passports))  # part 1 - 256
+print(sum(sum(1 if re.match(REGEX[f[:3]], f[4:]) else 0 for f in p if f[:3] != "cid") == 7 for p in passports))  # part 2 -  198
