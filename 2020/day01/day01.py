0bcaecd42445e49966a6a579b6e6fa3f5a284acd
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-30 04:56:13 -0500

    Modify solutions for Day 1 2020

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -1,19 +1,6 @@
+from itertools import combinations
+
 nums = list(map(int, open("input01.txt")))
-num_set = set(nums)  # set for O(1) lookup
-
-
-def part1():
-    for i in range(len(nums)):
-        if 2020 - nums[i] in num_set:
-            return nums[i] * (2020 - nums[i])
-
-
-def part2():
-    for i in range(len(nums) - 1):
-        for j in range(i + 1, len(nums)):
-            if 2020 - nums[i] - nums[j] in num_set:
-                return nums[i] * nums[j] * (2020 - nums[i] - nums[j])
-
-
-print(part1())  # 1019371
-print(part2())  # 278064990
+num_set = set(nums)  # set for fast lookup
+print(next(n * (2020 - n) for n in nums if 2020 - n in num_set))  # part 1 - 1019371
+print(next(a * b * (2020 - a - b) for a, b in combinations(nums, 2) if 2020 - a - b in num_set))  # part 2 - 278064990
