74c894f025c9d688ce7d333d2fc2eede38899fd6
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-30 05:16:37 -0500

    Modify solutions for Day 1 2020 a little more

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -2,5 +2,5 @@ from itertools import combinations
 
 nums = list(map(int, open("input01.txt")))
 num_set = set(nums)  # set for fast lookup
-print(next(n * (2020 - n) for n in nums if 2020 - n in num_set))  # part 1 - 1019371
-print(next(a * b * (2020 - a - b) for a, b in combinations(nums, 2) if 2020 - a - b in num_set))  # part 2 - 278064990
+print(next(a * b for a in nums if (b := 2020 - a) in num_set))  # part 1 - 1019371
+print(next(a * b * c for a, b in combinations(nums, 2) if (c := 2020 - a - b) in num_set))  # part 2 - 278064990
