b2e9799cf17b8e37bc727e97a07e910f5672e27a
Justin Doornbos <doornbosjustin@gmail.com>
2020-12-29 23:47:14 -0500

    Modify part 2 of Day 2 2018

--- 2018/day02/day02.py
+++ 2018/day02/day02.py
@@ -2,4 +2,4 @@ from itertools import combinations
 
 inp = list(map(str.strip, open("in02.txt")))
 print(sum(any(s.count(l) == 2 for l in set(s)) for s in inp) * sum(any(s.count(l) == 3 for l in set(s)) for s in inp))  # part 1 - 5727
-print(next(a[:l[0]] + a[l[0] + 1:] for a, b in combinations(inp, 2) if len(l := list(j for j, c in enumerate(zip(a, b)) if c[0] != c[1])) == 1))  # part 2 - uwfmdjxyxlbgnrotcfpvswaqh
+print(next("".join(l) for a, b in combinations(inp, 2) if len(l := list(c for c, d in zip(a, b) if c == d)) == len(a) - 1))  # part 2 - uwfmdjxyxlbgnrotcfpvswaqh
