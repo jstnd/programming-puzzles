1df38033d78e30d8a20d688c6bb0914e78e9c9f9
Justin Doornbos <doornbosjustin@gmail.com>
2024-10-06 11:06:00 -0400

    improve day 4 2015 solution [rust]

--- rust/src/year2015/day04.rs
+++ rust/src/year2015/day04.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use md5::Digest;
 
 pub fn get_input() -> String {
@@ -5,28 +7,35 @@ pub fn get_input() -> String {
 }
 
 pub fn part1(input: &str) -> i32 {
-    search(input.trim(), "00000")
+    search(input.trim(), 1)
 }
 
 pub fn part2(input: &str) -> i32 {
-    search(input.trim(), "000000")
+    search(input.trim(), 2)
 }
 
-fn search(key: &str, prefix: &str) -> i32 {
-    let mut number = 0;
+fn search(key: &str, part: u8) -> i32 {
+    let mut input = key.to_string();
+    let len = input.len();
+    let mut hasher = md5::Md5::new();
 
-    loop {
-        let str = format!(
-            "{:x}",
-            md5::Md5::digest(format!("{key}{number}").as_bytes())
-        );
+    for i in 0.. {
+        input.truncate(len);
+        write!(&mut input, "{}", i).unwrap();
+        hasher.update(&input);
 
-        if str.starts_with(prefix) {
-            break;
+        let hash = hasher.finalize_reset();
+
+        if part == 1 {
+            if hash[0] == 0 && hash[1] == 0 && hash[2] >> 4 == 0 {
+                return i;
+            }
+        } else {
+            if hash.starts_with(&[0, 0, 0]) {
+                return i;
+            }
         }
-
-        number += 1;
     }
 
-    number
+    unreachable!()
 }
