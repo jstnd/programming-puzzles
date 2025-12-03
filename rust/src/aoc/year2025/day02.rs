6f0c3a639c987ed75cc0a4ace9530ed100e79b3d
Justin Doornbos <doornbosjustin@gmail.com>
2025-12-02 20:17:04 -0500

    day 2 2025 -- add periodic string trick solution [aoc] [rust]

--- rust/src/aoc/year2025/day02.rs
+++ rust/src/aoc/year2025/day02.rs
@@ -1,6 +1,6 @@
 use std::ops::RangeInclusive;
 
-use fancy_regex::Regex;
+// use fancy_regex::Regex;
 
 use crate::util::num;
 
@@ -20,11 +20,33 @@ pub fn part2(input: &str) -> usize {
     // We can use regex here as a simple solution for finding a repeating pattern.
     // => (.+?) -> matches any character between one and unlimited times, as few times as possible
     // => \1+ -> matches the same text as found in the previous match between one and unlimited times, as many times as possible
-    let repeating = Regex::new("^(.+?)\\1+$").unwrap();
+    // let repeating = Regex::new("^(.+?)\\1+$").unwrap();
 
+    // parse(input)
+    //     .flatten()
+    //     .filter(|id| repeating.is_match(&id.to_string()).unwrap())
+    //     .sum()
+
+    // We can also use a different trick where for each id, we:
+    // 1. create a string with the id repeated twice
+    // 2. remove the first and last characters
+    // 3. check if the remaining string still contains the original id
+    //
+    // If the remaining string still contains the original id,
+    // that means the id has a repeating pattern.
+    // NOTE: see periodic strings
     parse(input)
         .flatten()
-        .filter(|id| repeating.is_match(&id.to_string()).unwrap())
+        .filter(|id| {
+            // An id that is a single digit won't fit the criteria, so we return early.
+            if num::len(*id) <= 1 {
+                return false;
+            }
+
+            let id = id.to_string();
+            let id_repeat = id.repeat(2);
+            id_repeat[1..id_repeat.len() - 1].contains(&id)
+        })
         .sum()
 }
 
